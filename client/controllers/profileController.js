e695002fe5e089196d1c17fe103be2d7fe238ba5
PestoP <f1zanard@enib.fr>
2016-08-08 15:18:22 +0200

    Style de la page profil

--- client/controllers/profileController.js
+++ client/controllers/profileController.js
@@ -1,3 +1,5 @@
-appBlog.controller('profileCtrl', [ function () {
-	// body...
+appBlog.controller('profileCtrl', [ '$scope', function ($scope) {
+	$scope.title = "Titre de l'article";
+	$scope.categorie = "Nature";
+	$scope.auteur = "Lavoisier";
 }]); 
